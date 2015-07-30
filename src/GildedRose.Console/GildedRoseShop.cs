d4475a8fb23bf0cc1d560c959d1bdf2f525bab4d
Dave Mateer <davemateer@gmail.com>
2015-07-30 08:03:23 +0100

    dave

---
+++ src/GildedRose.Console/GildedRoseShop.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRose.Console{
+    public class GildedRoseShop
+    {
+        public IList<Item> UpdateDailyQualities(IList<Item> items)
+        {
+            var itemsUpdated = new List<Item>();
+            foreach (Item item in items)
+            {
+                var result = UpdateItemQuality(item);
+                itemsUpdated.Add(result);
+            }
+            return itemsUpdated;
+        }
+
+        public Item UpdateItemQuality(Item item)
+        {
+            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (item.Quality > 0)
+                {
+                    if (item.Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        item.Quality = item.Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 1;
+
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (item.SellIn < 11)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
+                        }
+
+                        if (item.SellIn < 6)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                item.SellIn = item.SellIn - 1;
+            }
+
+            if (item.SellIn < 0)
+            {
+                if (item.Name != "Aged Brie")
+                {
+                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (item.Quality > 0)
+                        {
+                            if (item.Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                item.Quality = item.Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        item.Quality = item.Quality - item.Quality;
+                    }
+                }
+                else
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
+                }
+            }
+            return item;
+        }
+    }
+}
--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,13 +1,25 @@
+using GildedRose.Console;
 using Xunit;
 
 namespace GildedRose.Tests
 {
     public class TestAssemblyTests
     {
+        private readonly GildedRoseShop shop = new GildedRoseShop();
+
         [Fact]
-        public void TestTheTruth()
+        public void GivenDexterityVest_ShouldReduceItemQualityByOne()
         {
-            Assert.True(true);
+            var normalItem = GetNormalItemDexterityVest();
+            int startingQuality = normalItem.Quality;
+
+            var result = shop.UpdateItemQuality(normalItem);
+
+            Assert.Equal(startingQuality-1, result.Quality);
+        }
+
+        private static Item GetNormalItemDexterityVest(){
+            return new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 };
         }
     }
 }
